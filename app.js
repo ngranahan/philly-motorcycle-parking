42bda9a63fd2f29ee5ec64b8c65c8c49e1422046
Nora Granahan <nora@punkave.com>
2020-01-20 14:46:33 -0500

    Adds simple scraper and dumps data into an array

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require("express");
 const expressNunjucks = require('express-nunjucks');
 const bodyParser = require("body-parser");
 
+// TODO: Move these things over to another file
+const axios = require('axios');
+const cheerio = require('cheerio');
+const url = 'http://www.philapark.org/motorcycle-scooter-parking/';
+// 
+
 const app = express();
 const isDev = app.get('env') === 'development';
 
@@ -16,9 +22,24 @@ const njk = expressNunjucks(app, {
     watch: isDev,
     noCache: isDev
 });
+
+// TODO: Move this to another file
+axios.get(url).then((res) => {
+    // Scrape addresses and drop 'em in an array
+    const $ = cheerio.load(res.data);
+    const addresses = [];
+    $('li', '.span3').each(function(i, el) {
+        addresses[i] = $(this).text();
+    });
+    addresses.join(', ');
+    console.log(addresses);
+}).catch((err) => {
+    console.log(`there was an error: ${err}`);
+});
+// 
  
 app.get('/', (req, res) => {
-    res.render('index', {title: 'testing testing'});
+    res.render('index');
 });
 
 app.listen(PORT, () => console.log("App running on port " + PORT + " 💥"));
